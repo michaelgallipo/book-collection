5e6509b2cd12c26337cef6c48d11f4b3494fa39d
michael <michael.gallipo@gmail.com>
2019-02-25 23:57:56 -0500

    Added Redux, implemented saving author in state

--- src/App.js
+++ src/App.js
@@ -2,68 +2,85 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 import Books from "./Books";
+import { connect } from "react-redux";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { books: [] };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { books: [] };
+		console.log(props.searchTerm);
 
-  authorSubmit = e => {
-    e.preventDefault();
-    const author = e.target.elements.author.value;
-    console.log(author);
-    this.getBookData(author);
-  };
+		if (props.searchTerm !== undefined) {
+			this.getBookData(props.searchTerm);
+		}
+	}
 
-  getBookData = author => {
-    axios
-      .get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${author}`)
-      .then(response => {
-        const books = response.data.items.map(book => {
-          const volumeInfo = book.volumeInfo;
-          return {
-            title: volumeInfo.title,
-            author: volumeInfo.authors[0],
-            imageUrl: volumeInfo.imageLinks
-              ? volumeInfo.imageLinks.smallThumbnail
-              : null,
-            description: volumeInfo.description,
-            publishedDate: volumeInfo.publishedDate,
-            pageCount: volumeInfo.pageCount,
-            categories: volumeInfo.categories
-          };
-        });
-        this.setState({ books: books });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
+	authorSubmit = e => {
+		e.preventDefault();
+		const author = e.target.elements.author.value;
+		this.getBookData(author);
+		this.props.booksSearched(author);
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Book Collection</h2>
+	getBookData = author => {
+		axios
+			.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${author}`)
+			.then(response => {
+				const books = response.data.items.map(book => {
+					const volumeInfo = book.volumeInfo;
+					return {
+						title: volumeInfo.title,
+						author: volumeInfo.authors[0],
+						imageUrl: volumeInfo.imageLinks
+							? volumeInfo.imageLinks.smallThumbnail
+							: null,
+						description: volumeInfo.description,
+						publishedDate: volumeInfo.publishedDate,
+						pageCount: volumeInfo.pageCount,
+						categories: volumeInfo.categories
+					};
+				});
+				this.setState({ books: books });
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
 
-          <form onSubmit={this.authorSubmit}>
-            <div className="row justify-content-lg-center" id="data-entry">
-              <input
-                id="author"
-                className="form-control form-control-lg col-lg-6"
-                type="text"
-                name="author"
-                placeholder="Enter Author"
-              />
-            </div>
-            <button>Submit</button>
-          </form>
-        </header>
-        <Books books={this.state.books} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h2>Book Collection</h2>
+
+					<form onSubmit={this.authorSubmit}>
+						<div className="row justify-content-lg-center" id="data-entry">
+							<input
+								id="author"
+								className="form-control form-control-lg col-lg-6"
+								type="text"
+								name="author"
+								placeholder="Enter Author"
+							/>
+						</div>
+						<button>Submit</button>
+					</form>
+				</header>
+				<Books books={this.state.books} />
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = state => ({
+	...state
+});
+
+const mapDispatchToProps = dispatch => ({
+	booksSearched: author => dispatch({ type: "BOOKS_SEARCHED", payload: author })
+});
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import BookInfo from './BookInfo';
-import * as serviceWorker from './serviceWorker';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import BookInfo from "./BookInfo";
+import * as serviceWorker from "./serviceWorker";
+import { applyMiddleware, createStore } from "redux";
+import { createLogger } from 'redux-logger'
+import { Provider } from "react-redux";
+import searchReducer from "./reducers/searchReducer";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+const logger = createLogger();
+let store = createStore(searchReducer, applyMiddleware(logger));
 const RoutedApp = (
-  <Router>
-    <div>
-      <Route exact path="/" component={App}/>
-      <Route exact path="/book" component={BookInfo}/>
-    </div>
-  </Router>
-)
+	<Router>
+		<Provider store={store}>
+			<Route exact path="/" component={App} />
+			<Route exact path="/book" component={BookInfo} />
+		</Provider>
+	</Router>
+);
 
-ReactDOM.render(RoutedApp, document.getElementById('root'));
-
-
+ReactDOM.render(RoutedApp, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
