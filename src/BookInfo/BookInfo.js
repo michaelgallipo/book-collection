7955b5beeaa9ad9b310838abadb35590d951dacf
michael <michael.gallipo@gmail.com>
2019-03-11 20:57:05 -0400

    Added Google Books button, not working properly yet

--- src/BookInfo/BookInfo.js
+++ src/BookInfo/BookInfo.js
@@ -4,30 +4,47 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 const BookInfo = props => {
-  const book = props.location.book;
-  const { title, imageUrl, author, description, publishedDate } = book;
-  return (
-    <Container style={{ marginTop: "25px" }}>
-      <Row>
-        {book ? (
-          <>
-            <Col sm={2}>
-              <img src={imageUrl} alt="No Image Found" />
-            </Col>
-            <Col sm={{ span: 9, offset: 1 }}>
-              <h4>
-                {title}, by {author}
-              </h4>
-              <h6>{description}</h6>
-              <h6>{publishedDate}</h6>
-            </Col>
-          </>
-        ) : (
-          "Error rendering book info"
-        )}
-      </Row>
-    </Container>
-  );
+	const book = props.location.book;
+	const {
+		title,
+		imageUrl,
+		author,
+		description,
+		publishedDate,
+		pageCount,
+		googleLink
+	} = book;
+
+	return (
+		<Container style={{ marginTop: "25px" }}>
+			<Row>
+				{book ? (
+					<>
+						<Col sm={2}>
+							<img src={imageUrl} alt="No Image Found" />
+						</Col>
+						<Col sm={{ span: 9, offset: 1 }}>
+							<h4>
+								{title}, by {author}
+							</h4>
+							<h6>{description}</h6>
+							<h6>{pageCount} pages</h6>
+							<h6>{publishedDate}</h6>
+							<br />
+							<button onClick={openGoogle(googleLink)}>
+								Click to go to Google Books
+							</button>
+						</Col>
+					</>
+				) : (
+					"Error rendering book info"
+				)}
+			</Row>
+		</Container>
+	);
+};
+var openGoogle = e => {
+	window.open(e.googleLink);
 };
 
 export default BookInfo;
