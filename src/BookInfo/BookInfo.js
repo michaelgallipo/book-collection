83a2a913fd9d4138cf587080f2fd6c4ef0d4937f
gerry <gmercado6@gmail.com>
2019-03-25 21:01:13 -0700

    added link to navigate to books index, fixed book image border

--- src/BookInfo/BookInfo.js
+++ src/BookInfo/BookInfo.js
@@ -2,52 +2,57 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
 const BookInfo = props => {
-	const book = props.location.book;
-	const {
-		title,
-		imageUrl,
-		author,
-		description,
-		publishedDate,
-		pageCount,
-		googleLink
-	} = book;
+  const book = props.location.book;
+  const {
+    title,
+    imageUrl,
+    author,
+    description,
+    publishedDate,
+    pageCount,
+    googleLink
+  } = book;
 
-	// const openGoogle = e => {
-	// 	window.open(e.googleLink);
-	// };
+  // const openGoogle = e => {
+  // 	window.open(e.googleLink);
+  // };
 
-	return (
-		<Container style={{ marginTop: "25px" }}>
-			<Row>
-				{book ? (
-					<>
-						<Col sm={2}>
-							<img src={imageUrl} alt="No Cover Found" />
-						</Col>
-						<Col sm={{ span: 9, offset: 1 }}>
-							<h4>
-								{title}, by {author}
-							</h4>
-							<h6>{description}</h6>
-							<h6>{pageCount} pages</h6>
-							<h6>{publishedDate}</h6>
-							<br />
-							<button>
-								<a href={googleLink} target="_blank">
-									Click to go to Google Books
-								</a>
-							</button>
-						</Col>
-					</>
-				) : (
-					"Error rendering book info"
-				)}
-			</Row>
-		</Container>
-	);
+  return (
+    <Container style={{ marginTop: "25px" }}>
+      <Link style={{ marginBottom: "15px" }} to={{ pathname: "/" }}>
+        Back to Books Index
+      </Link>
+
+      <Row>
+        {book ? (
+          <>
+            <Col sm={2}>
+              <img src={imageUrl} alt="No Cover Found" />
+            </Col>
+            <Col sm={{ span: 9, offset: 1 }}>
+              <h4>
+                {title}, by {author}
+              </h4>
+              <h6>{description}</h6>
+              <h6>{pageCount} pages</h6>
+              <h6>{publishedDate}</h6>
+              <br />
+              <button>
+                <a href={googleLink} target="_blank">
+                  Click to go to Google Books
+                </a>
+              </button>
+            </Col>
+          </>
+        ) : (
+          "Error rendering book info"
+        )}
+      </Row>
+    </Container>
+  );
 };
 
 export default BookInfo;
--- src/Books/Books.js
+++ src/Books/Books.js
@@ -4,46 +4,49 @@ import Row from "react-bootstrap/Row";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Books = props => {
-	if (props.books.length > 0) {
-		setTimeout(() => {
-			const element = document.getElementById("bookList");
-			element.scrollIntoView({
-				behavior: "smooth",
-				block: "start",
-				inline: "nearest"
-			});
-		}, 1000);
-	}
+  if (props.books.length > 0) {
+    setTimeout(() => {
+      const element = document.getElementById("bookList");
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+      });
+    }, 1000);
+  }
 
-	return (
-		<div id="bookList">
-			<Row>
-				{props.books.length > 0
-					? props.books.map((book, index) => {
-							const imageUrl = book.imageUrl
-								? book.imageUrl
-								: "http://sbr.redcom.in/books/sites/default/files/default_images/no-cover-placeholder.jpg";
-							return (
-								<Col sm={4} key={index}>
-									<div className="card" style={{ width: "18rem" }}>
-										<Link to={{ pathname: "/book", book }}>
-											<img
-												className="card-img-top"
-												src={imageUrl}
-												alt="No Image Found"
-											/>
-										</Link>
-										<div className="card-body">
-											{book.title}, by {book.author}
-										</div>
-									</div>
-								</Col>
-							);
-					  })
-					: "no books found"}
-			</Row>
-		</div>
-	);
+  return (
+    <div id="bookList">
+      <Row>
+        {props.books.length > 0
+          ? props.books.map((book, index) => {
+              const imageUrl = book.imageUrl
+                ? book.imageUrl
+                : "http://sbr.redcom.in/books/sites/default/files/default_images/no-cover-placeholder.jpg";
+              return (
+                <Col sm={4} key={index}>
+                  <div
+                    className="card"
+                    style={{ width: "18rem", border: "none" }}
+                  >
+                    <Link to={{ pathname: "/book", book }}>
+                      <img
+                        className="card-img-top"
+                        src={imageUrl}
+                        alt="No Image Found"
+                      />
+                    </Link>
+                    <div className="card-body">
+                      {book.title}, by {book.author}
+                    </div>
+                  </div>
+                </Col>
+              );
+            })
+          : "no books found"}
+      </Row>
+    </div>
+  );
 };
 
 export default Books;
